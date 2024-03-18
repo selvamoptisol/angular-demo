a49b779c43c9dfd0af9e61ed3b39b55043a599f6
Selvam <selvam.m@optisolbusiness.com>
2024-03-18 14:55:51 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'user', loadChildren: () => import('./createuser/user.module').then(m => m.UserModule) },
+  { path: '', redirectTo:'user', pathMatch:"full" },
 
 ];
 
